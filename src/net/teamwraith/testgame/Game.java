9062efc8993436df2795a907eb07c7748e401913
Vegard Berg <vgberg@gmail.com>
2013-04-14 19:00:48 +0200

    Fixed capitalisation for final variables.

--- src/net/teamwraith/testgame/Game.java
+++ src/net/teamwraith/testgame/Game.java
@@ -5,24 +5,24 @@ import org.newdawn.slick.state.*;
 
 public class Game extends StateBasedGame{
 
-	public static final String gamename = "TESTGAME";
-	public static final int menu = 0;
-	public static final int play = 1;
+	public static final String GAMENAME = "TESTGAME";
+	public static final int MENU = 0;
+	public static final int PLAY = 1;
 
-	public Game(String gamename){
-		super(gamename);
-		this.addState(new Menu(menu));
+	public Game(String GAMENAME){
+		super(GAMENAME);
+		this.addState(new Menu(MENU));
 	}
 	
 	public void initStatesList(GameContainer gc) throws SlickException {
-		this.getState(menu).init(gc, this);
-		this.enterState(menu);
+		this.getState(MENU).init(gc, this);
+		this.enterState(MENU);
 	}
 	
 	public static void main(String[] args) {
 		AppGameContainer appgc;
 		try{
-			appgc = new AppGameContainer(new Game(gamename));
+			appgc = new AppGameContainer(new Game(GAMENAME));
 			appgc.setDisplayMode(1280, 720, false);
 			appgc.start();
 		}catch(SlickException e){
