854dcf5dd7ea3bcd522203e24759eedb46426fe8
Birk Solbjorg <infinitefacepalm@gmail.com>
2013-04-18 15:50:52 +0200

    Added .ini file

--- src/net/teamwraith/testgame/Game.java
+++ src/net/teamwraith/testgame/Game.java
@@ -1,5 +1,13 @@
 package net.teamwraith.testgame;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
@@ -8,22 +16,40 @@ public class Game extends StateBasedGame{
 	public static final String GAME_NAME = "TESTGAME";
 	public static final int MENU = 0;
 	public static final int PLAY = 1;
+	public static Properties properties = new Properties();
 	
 	public Game(final String GAME_NAME){
 		super(GAME_NAME);
 		this.addState(new Menu(MENU));
 	}
 	
-	public void initStatesList(GameContainer gc) throws SlickException {
-		this.getState(MENU).init(gc, this);
+	public void initStatesList(GameContainer gc) {
+		try {
+			this.getState(MENU).init(gc, this);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 		this.enterState(MENU);
 	}
 	
 	public static void main (String[] args) {
 		AppGameContainer appgc;
+		
+		try {
+			FileInputStream reader = new FileInputStream("game.ini");
+			properties.load(reader);
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
 		try {
 			appgc = new AppGameContainer(new Game(GAME_NAME));
-			appgc.setDisplayMode(1280, 720, false);
+			appgc.setDisplayMode(
+				Integer.parseInt(properties.getProperty("width")), 
+				Integer.parseInt(properties.getProperty("height")), 
+				Boolean.parseBoolean(properties.getProperty("fullscreen"))
+			);
 			appgc.start();
 		} catch(SlickException e) {
 			e.printStackTrace();
